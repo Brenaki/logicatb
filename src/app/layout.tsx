a644cfd929850509000b18721fdefcc6e59c0819
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2024-06-21 02:54:07 -0300

    [Add] Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LogicTb",
+  title: "LOLGIX",
   description: "Elaborar uma página web que descreva e exemplifique os seguintes tópicos: inferência proposicional com raciocínio para frente, unificação em lógica de primeira ordem, raciocínio para trás em lógica de primeira ordem e forma normal prenex. A tarefa deve ser realizada em grupos de 3 ou 4 alunos.",
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,11 +8,13 @@ import { conteudos } from "@/constants";
 export function Navbar() {
 
   return (
-    <nav className="flexCenter flex-col gap-y-4 p-4 mb-20">
-      <Link href="/" className="flexCenter gap-x-2">
-        <Brand />
-        <h1 className="text-3xl font-bold text-">LOLGIX</h1>
-      </Link>
+    <nav className="flexCenter flex-col gap-y-4 mb-28">
+      <div className="min-w-full bg-gray-40 p-4">
+        <Link href="/" className="flexCenter gap-x-2">
+          <Brand />
+          <h1 className="text-3xl font-bold text-gray-100">LOLGIX</h1>
+        </Link>
+      </div>
       <ul className="flexCenter gap-x-8 relative">
         <li>
           <DropdownMenu.Root>
@@ -20,9 +22,9 @@ export function Navbar() {
               Conteudos ▼
             </DropdownMenu.Trigger>
             <DropdownMenu.Content className="p-2 rounded-md shadow-xl">
-              <DropdownMenu.Item className="p-1"><Link href={conteudos[0].href}>{conteudos[0].title}</Link></DropdownMenu.Item>
-              <DropdownMenu.Item className="p-1"><Link href={conteudos[1].href}>{conteudos[1].title}</Link></DropdownMenu.Item>
-              <DropdownMenu.Item className="p-1"><Link href={conteudos[2].href}>{conteudos[2].title}</Link></DropdownMenu.Item>
+              <DropdownMenu.Item className="p-2 hover:bg-gray-10"><Link href={conteudos[0].href}>{conteudos[0].title}</Link></DropdownMenu.Item>
+              <DropdownMenu.Item className="p-2 hover:bg-gray-10"><Link href={conteudos[1].href}>{conteudos[1].title}</Link></DropdownMenu.Item>
+              <DropdownMenu.Item className="p-2 hover:bg-gray-10"><Link href={conteudos[2].href}>{conteudos[2].title}</Link></DropdownMenu.Item>
             </DropdownMenu.Content>
           </DropdownMenu.Root>
         </li>
